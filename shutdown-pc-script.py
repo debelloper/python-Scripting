903205029777002cace5e9047eb6dd2eefbdb16b
tech-Bello <muhammard2k01bello@gmail.com>
2026-01-12 00:25:19 +0100

    this is my first scripts

--- shutdown-pc-script.py
+++ shutdown-pc-script.py
@@ -1,16 +1,19 @@
 import os
 import platform
 import sys
+import subprocess
 
 
 # Function to shut down the PC
 def shutdown_pc(): 
+
     sys_platform = platform.system()
     
     if platform.system() == "Linux" or platform.system() == "Darwin":  # macOS is identified as 'Darwin'
-        os.system("sudo shutdown -h now")
+        subprocess.run(["sudo", "shutdown", "-h", "now"])
     else: # Assuming Windows
         os.system("shutdown /s /t 0")
 
 if __name__ == "__main__":
     shutdown_pc()
+# Note: This script requires administrative privileges to execute the shutdown command.
