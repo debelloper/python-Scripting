517f1e33959257f14ac3161ae4d7fc3c4d2b1088
tech-Bello <muhammard2k01bello@gmail.com>
2026-01-13 23:13:18 +0100

    update

--- search-script.py
+++ search-script.py
@@ -2,3 +2,12 @@ import os # For interacting with the operating system
 import sys # For accessing command-line arguments
 import fnmatch # For pattern matching
 import argparse # For parsing command-line arguments
+
+def search_files(directory, pattern):
+    """Search for files matching the given pattern in the specified directory and its subdirectories."""
+    matches = []
+    for root, dirs, files in os.walk(directory):
+        for filename in files:
+            if fnmatch.fnmatch(filename, pattern):
+                matches.append(os.path.join(root, filename))
+    return matches
