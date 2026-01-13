d156edaa7928db7e0793455144d9492c73f441a3
tech-Bello <muhammard2k01bello@gmail.com>
2026-01-13 23:11:34 +0100

    script for searching file

---
+++ search-script.py
@@ -0,0 +1,4 @@
+import os # For interacting with the operating system
+import sys # For accessing command-line arguments
+import fnmatch # For pattern matching
+import argparse # For parsing command-line arguments
