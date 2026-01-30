8605631798065615b55d02c25ca8c66ad728e78b
tech-Bello <muhammard2k01bello@gmail.com>
2026-01-30 21:06:59 +0100

    added new scripting file

--- password-enc-n-denc.py
+++ password-enc-n-denc.py
@@ -2,7 +2,7 @@
 from cryptography.fernet import Fernet # type: ignore
 
 def encrypt(text, key):
-    f = Fernet(key)
+    f = Fernet(key) # fernet object creation
     encrypted_text = f.encrypt(text.encode())
     return encrypted_text
 
@@ -11,7 +11,7 @@ def decrypt(encrypted_text, key):
     decrypted_text = f.decrypt(encrypted_text).decode()
     return decrypted_text
 
-text = "user_text"
+text = input("Enter text to encrypt: ")
 key = Fernet.generate_key()
 encrypted_text = encrypt(text, key)
 print("Encrypted text: ", encrypted_text)
