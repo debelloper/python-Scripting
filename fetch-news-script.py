ef9b60eaf508eb388f2f12d7ebdda424b49896b5
tech-Bello <muhammard2k01bello@gmail.com>
2026-01-31 19:35:32 +0100

    added my api keys

---
+++ fetch-news-script.py
@@ -0,0 +1,15 @@
+# $ pip install newsapi-python
+from newsapi import NewsApiClient # type: ignore
+
+newsapi = NewsApiClient(api_key='3a51f959d978424fa0cf2e82e94a6675')
+
+# /v2/top-headlines
+top_headlines = newsapi.get_top_headlines(language='en', country='in')
+
+if top_headlines:
+    for article in top_headlines["articles"]:
+        title = article["title"]
+        print(f"Title: {title}\n")
+else:
+    print("No articles found.")
+
