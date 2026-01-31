676d4f19b8592ec79e8f607cacc29989b5730c76
tech-Bello <muhammard2k01bello@gmail.com>
2026-01-31 19:39:44 +0100

    debugging d code

--- fetch-news-script.py
+++ fetch-news-script.py
@@ -2,7 +2,8 @@
 from newsapi import NewsApiClient # type: ignore
 
 newsapi = NewsApiClient(api_key='3a51f959d978424fa0cf2e82e94a6675')
-
+print("Fetching top headlines...", end="\n\n")
+print(newsapi)
 # /v2/top-headlines
 top_headlines = newsapi.get_top_headlines(language='en', country='in')
 
