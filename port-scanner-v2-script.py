b4978ec89c14d080c2641f9ccaa4c47fd73fb8b5
tech-Bello <muhammard2k01bello@gmail.com>
2026-03-14 22:43:50 +0100

    added v2 of port-scanner script

---
+++ port-scanner-v2-script.py
@@ -0,0 +1,18 @@
+import socket
+import threading
+
+target = input("Enter target IP: ")
+ports = range(1, 1025)
+
+def scan(port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.setdefaulttimeout(1)
+
+    if s.connect_ex((target, port)) == 0:
+        print(f"[+] Port {port} open")
+
+    s.close()
+
+for port in ports:
+    thread = threading.Thread(target=scan, args=(port,))
+    thread.start()
