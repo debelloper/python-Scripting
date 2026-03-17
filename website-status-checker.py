a54a838a6d45cc05601ff437f2af2482ab5f5cda
tech-Bello <muhammard2k01bello@gmail.com>
2026-03-17 22:06:05 +0100

    added if name is main

--- website-status-checker.py
+++ website-status-checker.py
@@ -2,10 +2,17 @@ import requests # pip install requests
 
 sites = ["https://google.com", "https://github.com"] # Add more sites to check here
 
-for site in sites:
+def check_website_status(site):
     try:
         r = requests.get(site)
-        print(site, "UP")
+        return True
     except:
+        return False
+for site in sites:
+    if check_website_status(site):
+        print(site, "UP")
+    else:
         print(site, "DOWN")
-        
+
+if __name__ == "__main__":
+    print("Website status check completed.")
